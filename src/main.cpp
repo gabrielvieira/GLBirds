4066bcdcbe2763574c6062a648f2558eaa295dab
Gabriel Vieira <viera_frifri@hotmail.com>
2017-10-25 01:34:10 -0200

    ref link for background render

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,4 @@
-#ifdef __APPLE__
-#  include <OpenGL/gl.h>
-#  include <OpenGL/glu.h>
-#  include <GLUT/glut.h>
-#else
-#  include <GL/gl.h>
-#  include <GL/glu.h>
-#  include <GL/glut.h>
-#endif
-
-#include <stdio.h>
+#include "main.h"
 
 GLuint texture = 0;
 void background()
